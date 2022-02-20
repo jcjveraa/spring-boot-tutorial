7509e57964ec15ff702cecc951e5bef2411fd7c4
JelleV <3942301+jcjveraa@users.noreply.github.com>
2022-02-20 12:07:11 +0100

    migration to Eclipse project

--- README.md
+++
@@ -1,2 +0,0 @@
-# spring-tutorial
-Repository to follow along with https://www.udemy.com/course/spring-hibernate-tutorial/
--- src/com/luv2code/springdemo/MyLoggerConfig.java
+++ src/com/luv2code/springdemo/MyLoggerConfig.java
@@ -4,7 +4,7 @@ import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
- 
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
  
 public class MyLoggerConfig {
