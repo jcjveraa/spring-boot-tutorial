479a16d54bf568c67f11cf6e62759fa32539bfbf
JelleV <3942301+jcjveraa@users.noreply.github.com>
2022-02-24 19:11:28 +0100

    71

--- spring-demo-annotations/src/com/luv2code/springdemo/RandomFortuneService.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/RandomFortuneService.java
@@ -1,13 +1,19 @@
 package com.luv2code.springdemo;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomFortuneService implements FortuneService {
 
+    String[] fortunes = {"Random fortune 1!", "Random fortune 2!", "Here be Dragons!"};
+
     @Override
     public String getFortune() {
-        return "Random fortune!";
+        Random random = new Random();
+        int randomIndex = random.nextInt(fortunes.length);
+        return fortunes[randomIndex];
     }
     
 }
