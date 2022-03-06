6f1f6c6d6de583c3afa97a294d8920e2f1892790
JelleV <3942301+jcjveraa@users.noreply.github.com>
2022-03-06 16:24:44 +0100

    deployment success...

---
+++ 101-spring-mvc/deploy-scripts/deploy.ps1
@@ -0,0 +1,12 @@
+Remove-Item .\tomcat\tomcat-webapps\*.war 
+Copy-Item -Path .\WebContent -Destination .\deploy\Webcontent -Container -Recurse
+Copy-Item -Path ..\spring_lib\spring*.jar -Destination .\deploy\WebContent\WEB-INF\lib
+Copy-Item -Path .\bin -Recurse -Destination .\deploy\WebContent\WEB-INF\classes
+
+# Compress-Archive -Path .\deploy\WebContent -DestinationPath .\deploy\spring-mvc-demo.zip -CompressionLevel Fastest 
+.\deploy-scripts\7zip\7za.exe a -r .\deploy\spring-mvc-demo.zip .\deploy\WebContent\*
+Rename-Item -Path .\deploy\spring-mvc-demo.zip -NewName spring-mvc-demo.war
+
+Copy-Item -Path .\deploy\spring-mvc-demo.war -Destination .\tomcat\tomcat-webapps
+# clean deployment folder
+Remove-Item .\deploy -Recurse
